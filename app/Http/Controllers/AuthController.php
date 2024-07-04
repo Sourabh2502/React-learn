ee024d530fce960db225aa22d9139d48dd7c676f
sourabh <aerosourabh92@gmail.com>
2024-07-04 18:04:33 +0530

    add token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,36 +1,33 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\RegUser;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Laravel\Passport\HasApiTokens;
 
 class AuthController extends Controller
 {
+    use HasApiTokens;
 
     public function register(Request $request)
     {
-        // Validate incoming request
         $validatedData = $request->validate([
-            'email' => 'required|email|unique:users',
+            'email' => 'required|email|unique:regusers',
             'password' => 'required|min:6',
         ]);
 
-        // Create and store the new user
         $user = new RegUser();
         $user->email = $validatedData['email'];
         $user->password = Hash::make($validatedData['password']);
         $user->save();
 
-        // Optionally, return some response
         return response()->json(['message' => 'User registered successfully'], 201);
     }
 
     public function login(Request $request)
     {
-        // Validate incoming request
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required',
@@ -40,16 +37,14 @@ class AuthController extends Controller
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
-        // Check if user exists in the reguser table
         $user = Reguser::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response()->json(['message' => 'Invalid email or password'], 401);
         }
 
-        // User exists and password is correct
-        return response()->json(['message' => 'Login successful'], 200);
+        $token = $user->createToken('Personal Access Token')->accessToken;
+
+        return response()->json(['token' => $token], 200);
     }
-
 }
-
--- app/Models/RegUser.php
+++ app/Models/RegUser.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class RegUser extends Model
 {
     use HasFactory;
+     use HasApiTokens;
     protected $table='reguser';
     protected $fillable = [ 'email','password'];
 }
